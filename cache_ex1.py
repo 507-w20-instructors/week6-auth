a52d9b1349f856ccade08c85e55dfc34b213d18b
Mark Newman <mwnewman@umich.edu>
2020-02-13 00:56:15 -0500

    twitter and caching, commit 1

---
+++ cache_ex1.py
@@ -0,0 +1,9 @@
+
+
+def fib(n):
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        fib_seq.append(fib_seq[i - 2] + fib_seq[i - 1])
+    return fib_seq[-1]
+
+print (fib(300000))
--- twit.py
+++ twit.py
@@ -1,25 +1,25 @@
 import json
-from requests_oauthlib import OAuth1Session
+import requests
+from requests_oauthlib import OAuth1
+
 import secrets
 
 client_key = secrets.TWITTER_API_KEY
 client_secret = secrets.TWITTER_API_SECRET
 
-resource_owner_key = secrets.TWITTER_ACCESS_TOKEN
-resource_owner_secret = secrets.TWITTER_ACCESS_TOKEN_SECRET
+access_token = secrets.TWITTER_ACCESS_TOKEN
+access_token_secret = secrets.TWITTER_ACCESS_TOKEN_SECRET
 
+oauth = OAuth1(client_key,
+            client_secret=client_secret,
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret)
 
-oauth = OAuth1Session(client_key,
-                          client_secret=client_secret,
-                          resource_owner_key=resource_owner_key,
-                          resource_owner_secret=resource_owner_secret)
+endpoint_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q': '@umsi'}
+response = requests.get(endpoint_url, params=params, auth=oauth)
 
-protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
-params = {'q':'food'}
-r = oauth.get(protected_url, params=params)
-results = r.json()
-#print(json.dumps(results, indent=2))
-
+results = response.json()
 tweets = results['statuses']
 for t in tweets:
     print(t['text'])
